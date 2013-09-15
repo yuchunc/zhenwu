08c0f5a2478298ad20aca07585b620db1f5db096
Mickey Home <poverwhelming@gmail.com>
2013-09-15 09:42:13 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zhenwu::Application.config.secret_key_base = '80ea6a440c2fa996d6c415cfd2f46422a1fe00551115657e2c2cef45612d64b73ed313b045c88027951cc68d49cc334f3a8a0004b0adbb84da6e8cafa98832c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zhenwu::Application.config.session_store :cookie_store, key: '_zhenwu_session'

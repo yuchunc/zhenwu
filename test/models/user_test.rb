e80903c3553ab3602f99d2171f2140b84e60a3bd
Mickey Home <poverwhelming@gmail.com>
2013-09-23 08:27:19 +0800

    start to try some test from rails guide, looks fun

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
+  test "the truth" do
+    assert true
+  end
+
+  test "should not save user without email" do
+    user = User.new
+    refute user.save
+  end
+
+  # test "should report some errors" do
+  #   some_undefined_variable
   #   assert true
   # end
+
 end

f9738c318aa5939573b7b98e7510c75d62e41a07
Mickey <mickey@bbin.com>
2013-09-23 17:30:39 +0800

    added page migration

--- db/migrate/20130923092336_create_pages.rb
+++ db/migrate/20130923092336_create_pages.rb
@@ -1,6 +1,14 @@
 class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
+      t.string  :name
+      t.string  :title
+      t.string  :description
+      t.text    :content
+      t.boolean :immortal
+      t.string  :language
+      t.integer :pagetype, default: 0
+      t.integer :page_id
 
       t.timestamps
     end

5eac99201811464cdf34bd536d7c70d860adc97e
Mickey Home <poverwhelming@gmail.com>
2013-09-29 22:13:45 +0800

    yay, first code done by tdd

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ActiveRecord::Base
 
   validates_presence_of :title
+
+  def self.find_translated(name, language)
+    self.find_by( name: name, language: language)
+  end
 end
--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -14,11 +14,18 @@ describe Page do
 
   describe "find page by language" do
     before do
-      @page_en = FactoryGirl.create(:page_en)
-      @page_tw = FactoryGirl.create(:page_zh_tw)
+      @page_en = FactoryGirl.create(:page_en, name: "test")
+      @page_tw = FactoryGirl.create(:page_zh_tw, name: "test")
     end
 
-    it "should find the page according to page name and title" do
+    it "should find English page by page name and language" do
+      @page = Page.find_translated("test", :en)
+      @page.must_equal @page_en
+    end
+
+    it "should find Taiwan page by page name and language" do
+      @page = Page.find_translated("test", :"zh-TW")
+      @page.must_equal @page_tw
     end
   end
 

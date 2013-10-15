835017399324d29dac56ef46d4ab6b3b41c83508
Mickey Home <poverwhelming@gmail.com>
2013-10-15 17:48:36 +0800

    added and modified the toolbar elements

---
+++ app/assets/javascripts/ckeditor_config.js
@@ -0,0 +1,38 @@
+CKEDITOR.config.toolbar = 'ZhenWu';
+
+CKEDITOR.config.toolbar_ZhenWu =
+[
+  { name: 'document', items : [ 'Source','-','Preview','Templates' ] },
+  { name: 'clipboard', items : [ 'Undo','Redo','PasteFromWord' ] },
+  { name: 'basicstylestyles', items : [ 'Bold','Italic','Underline','Strike' ] },
+  { name: 'paragraph', items : [ 'NumberedListist','BulletedList','-','Outdent','Indent','-','Blockquote',
+          '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock' ] },
+  { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+  { name: 'insert', items : [ 'Image','Table','HorizontalRule','SpecialChar' ] },
+  { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+  { name: 'colors', items : [ 'TextColor','BGColor' ] },
+  { name: 'tools', items : [ 'Maximize', 'ShowBlocks' ] }
+];
+
+/*full setting
+CKEDITOR.config.toolbar_Full =
+[
+  { name: 'document', items : ['Source','-','Save','NewPage','DocProps','Preview','Print','-','Templates' ] },
+  { name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+  { name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+  { name: 'formsrms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton',
+          'HiddenField' ] },
+  '/',
+  { namee: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-',
+           'RemoveFormat' ] },
+  { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
+          '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+  { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+  { name: 'insert', items : [ 'Image','Flash','Table','HiddenField','HorizontalRule','Smiley','SpecialChar',
+          'PageBreak','Iframe' ] },
+  '/',
+  { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+  { name: 'colors', items : [ 'TextColor','BGColor' ] },
+  { name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+];
+*/
